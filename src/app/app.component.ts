bb3dcc6ead5d6fd8bed79207ecf6d10511b32781
cyborg-s <nysascha1@gmail.com>
2025-02-24 12:01:44 +0100

    links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {LandingPageComponent} from './landing-page/landing-page.component'
+import { NavBarComponent } from './landing-page/nav-bar/nav-bar.component';
+import { FooterComponent } from './landing-page/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
